4966ef3deb0b35af7f680c14673c2cdae390b2c0
chohj1111 <58474921+chohj1111@users.noreply.github.com>
2019-12-04 23:38:12 +0900

    Create BFS_queue_AdjArr.cpp

---
+++ baseCode/BFS_queue_AdjArr.cpp
@@ -0,0 +1,15 @@
+void bfs(){
+  queue <int> q; 
+  check[1] = true;
+  q.push(1);
+  while(!q.empty()) {
+    int x =q.front();
+    q.pop();
+    for(int i=1;i<=n;i++){
+      if(a[x][i] ==1 && check[i] = false
+        check[i] = true;
+        q.push[i];
+        }
+    }
+  }
+}
