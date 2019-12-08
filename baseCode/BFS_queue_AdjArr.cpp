d5c855fe968532d56c94fb8b65df1c94fd9ed4e8
조 현준 <chohj1111>
2019-12-08 14:31:12 +0000

    a

--- baseCode/BFS_queue_AdjArr.cpp
+++ baseCode/BFS_queue_AdjArr.cpp
@@ -6,7 +6,7 @@ void bfs(){
     int x =q.front();
     q.pop();
     for(int i=1;i<=n;i++){
-      if(a[x][i] ==1 && check[i] = false
+      if(a[x][i] ==1 && check[i] = false) {
         check[i] = true;
         q.push[i];
         }
