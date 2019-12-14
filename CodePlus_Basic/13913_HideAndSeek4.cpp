c724d45fb6ae1af6848943e305e445ada7fd0091
Your Name <you@example.com>
2019-12-14 13:37:52 +0000

    191214

--- CodePlus_Basic/13913_HideAndSeek4.cpp
+++ CodePlus_Basic/13913_HideAndSeek4.cpp
@@ -4,11 +4,13 @@
 #include <queue>
 #include <cstring>
 #include <algorithm>
+#include <stack>
 #define LIMIT 100001
 
 using namespace std;
 int check[LIMIT];
 int dist[LIMIT];
+int from[LIMIT];
 int n, k;
 void bfs(int a){
   	queue <int> q; 
@@ -22,16 +24,19 @@ void bfs(int a){
 			q.push(x+1);
 			check[x+1] = true;
 			dist[x+1]= dist[x] +1;
+			from[x+1] = x;
 		}
 		if(x-1>=0&&check[x-1] == false) {
 			q.push(x-1);
 			check[x-1] = true;
 			dist[x-1] = dist[x] +1;
+			from[x-1] = x;
 		}
 		if(x*2<LIMIT&&check[x*2] == false) {
 			q.push(x*2);
 			check[x*2]= true;
 			dist[x*2] = dist[x] +1;
+			from[x*2] = x;
 		}
 	}
 }
@@ -43,5 +48,17 @@ int main(){
 	cin>> n>>k;
 	bfs(n);
 	cout<< dist[k]<<'\n';
+	int last = k;
+	stack<int> ans;
+	while(last != n){
+		ans.push(last);
+		last = from[last];
+	}
+	ans.push(n);
+	while(!ans.empty()){
+		cout<<ans.top()<<" ";
+		ans.pop();
+	}
+	cout<<'\n';
 	return 0;
 }
